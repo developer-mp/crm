acb941a88684dd3b0701c98bd6dbb1836101ec04
Mikita Piastou <piastoumikita@gmail.com>
2023-05-09 22:20:40 -0600

    Added Edit functionality

--- client/src/app/detail/Detail.js
+++ client/src/app/detail/Detail.js
@@ -1,22 +1,74 @@
 import List from "../../component/list/List.js";
 import Accordion from "../../component/accordion/Accordion.js";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import "./Detail.css";
 
 const Detail = () => {
   const { detail } = useSelector((store) => store.detail);
   const { result } = useSelector((store) => store.result);
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editedData, setEditedData] = useState(detail);
+
+  const handleEditClick = () => {
+    setIsEditMode(true);
+    setEditedData(detail);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditMode(false);
+    setEditedData(detail);
+  };
+
+  // const handleSaveClick = () => {
+  //   dispatch(updateDetail(editedData));
+  //   setIsEditMode(false);
+  // };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setEditedData({ ...editedData, [name]: value });
+  };
 
   return (
     <div className="detail">
       <List title={"Details: " + detail.customer}>
         {result.detail?.[0].panel.map((item) => (
           <Accordion title={item.title} key={item.title}>
-            {item.data.map((d) => (
-              <div key={d.label}>
-                <b>{d.label}</b> : {detail[d.dataField]}
+            {isEditMode ? (
+              <div>
+                <button className="detail-button" onClick={handleCancelClick}>
+                  Cancel
+                </button>
+                <button
+                  className="detail-button"
+                  // onClick={handleSaveClick}
+                >
+                  Save
+                </button>
               </div>
-            ))}
+            ) : (
+              <button className="detail-button" onClick={handleEditClick}>
+                Edit
+              </button>
+            )}
+            {item.data.map((d) =>
+              isEditMode ? (
+                <div key={d.label}>
+                  <b>{d.label}</b> :{" "}
+                  <input
+                    type="text"
+                    name={d.dataField}
+                    value={editedData[d.dataField]}
+                    onChange={handleInputChange}
+                  />
+                </div>
+              ) : (
+                <div key={d.label}>
+                  <b>{d.label}</b> : {detail[d.dataField]}
+                </div>
+              )
+            )}
           </Accordion>
         ))}
       </List>
